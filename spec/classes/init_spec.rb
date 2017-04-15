b5ef15b00a3a702ac8d4c90836398744f6c8aeef
Tim Meusel <tim@bastelfreak.de>
2017-04-15 16:28:01 +0200

    rubocop: autofixes

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -5,39 +5,42 @@ describe 'homeassistant' do
       let(:facts) do
         facts
       end
+
       context 'with defaults for all parameters' do
-        it { should contain_class('homeassistant') }
-        it { should contain_user('homeassistant').with_home('/srv/homeassistant') }
-        it { should contain_user('homeassistant').with_system(true) }
-        it { should contain_user('homeassistant').with_gid('homeassistant') }
-        it { should contain_group('homeassistant').with_system(true) }
-        it { should contain_file('/etc/homeassistant').with_ensure('directory') }
-        it { should contain_file('/etc/homeassistant').with_owner('homeassistant') }
-        it { should contain_class('python') }
-        it { should contain_python__pyvenv('/srv/homeassistant') }
-        it { should contain_systemd__unit_file('homeassistant.service').with_content(%r{^User=homeassistant}) }
-        it { should contain_systemd__unit_file('homeassistant.service').with_content(%r{^ExecStart=/srv/homeassistant/bin/hass -c "/etc/homeassistant"}) }
-        it { should contain_service('homeassistant').with_enable(true) }
+        it { is_expected.to contain_class('homeassistant') }
+        it { is_expected.to contain_user('homeassistant').with_home('/srv/homeassistant') }
+        it { is_expected.to contain_user('homeassistant').with_system(true) }
+        it { is_expected.to contain_user('homeassistant').with_gid('homeassistant') }
+        it { is_expected.to contain_group('homeassistant').with_system(true) }
+        it { is_expected.to contain_file('/etc/homeassistant').with_ensure('directory') }
+        it { is_expected.to contain_file('/etc/homeassistant').with_owner('homeassistant') }
+        it { is_expected.to contain_class('python') }
+        it { is_expected.to contain_python__pyvenv('/srv/homeassistant') }
+        it { is_expected.to contain_systemd__unit_file('homeassistant.service').with_content(%r{^User=homeassistant}) }
+        it { is_expected.to contain_systemd__unit_file('homeassistant.service').with_content(%r{^ExecStart=/srv/homeassistant/bin/hass -c "/etc/homeassistant"}) }
+        it { is_expected.to contain_service('homeassistant').with_enable(true) }
       end
       context 'with all parameters set' do
-        let(:params) do {
-          :home         => '/foo',
-          :config       => '/etc/bar',
-          :user         => 'steve',
-          :group        => 'friday',
-         }  
+        let(:params) do
+          {
+            home: '/foo',
+            config: '/etc/bar',
+            user: 'steve',
+            group: 'friday'
+          }
         end
-        it { should contain_group('friday') }
-        it { should contain_user('steve').with_gid('friday') }
-        it { should contain_user('steve').with_home('/foo') }
-        it { should contain_user('steve').with_system(true) }
-        it { should contain_file('/etc/bar').with_ensure('directory') }
-        it { should contain_file('/etc/bar').with_owner('steve') }
-        it { should contain_class('python') }
-        it { should contain_python__pyvenv('/foo') }
-        it { should contain_systemd__unit_file('homeassistant.service').with_content(%r{^User=steve}) }
-        it { should contain_systemd__unit_file('homeassistant.service').with_content(%r{^ExecStart=/foo/bin/hass -c "/etc/bar"}) }
-        it { should contain_service('homeassistant').with_enable(true) }
+
+        it { is_expected.to contain_group('friday') }
+        it { is_expected.to contain_user('steve').with_gid('friday') }
+        it { is_expected.to contain_user('steve').with_home('/foo') }
+        it { is_expected.to contain_user('steve').with_system(true) }
+        it { is_expected.to contain_file('/etc/bar').with_ensure('directory') }
+        it { is_expected.to contain_file('/etc/bar').with_owner('steve') }
+        it { is_expected.to contain_class('python') }
+        it { is_expected.to contain_python__pyvenv('/foo') }
+        it { is_expected.to contain_systemd__unit_file('homeassistant.service').with_content(%r{^User=steve}) }
+        it { is_expected.to contain_systemd__unit_file('homeassistant.service').with_content(%r{^ExecStart=/foo/bin/hass -c "/etc/bar"}) }
+        it { is_expected.to contain_service('homeassistant').with_enable(true) }
       end
     end
   end
